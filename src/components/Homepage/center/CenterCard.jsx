0f95646967e8da99b90cd20986d5799e6df60039
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-05-10 18:26:01 +0530

    center card changes

--- src/components/Homepage/center/CenterCard.jsx
+++ src/components/Homepage/center/CenterCard.jsx
@@ -7,6 +7,7 @@ const CenterCard = () => {
       <div className="center-Card">
         <img
           src="https://s3-alpha-sig.figma.com/img/c086/323e/b8fae54c91face434e9ea448a875ea3e?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CPPJu~LH5jYqxc8GcYbYi8GpYZgu3j-AFGk6P5AmeOJnOVh~MFFvaY-W2zLzP1Z-nbTnsFqaK9IwMhqY17hc~mTOe-T65fjKPsSLz2IuGlJPXLjCD0Nft~wq1uZntYN3HhCA5uVhv~0Et663QLCcv3lOLUCpuJFYrd1zDpR94UWGVhMmI-PnyW1EWDkGgfPIdMIBlyMQ5X9F7n2nNNI55Hp56B3F~tOPgUcBN9jDOTv2kKq1BuNYd-QxrPNSlQ~iJKerpafsOWRN14nu-xFnt8KbvT-IrBC3BatAs~MMcHy5IJBnBrAol9VY6q2kPz1p1ifSVXMX3tnrb9N4L74Zpg__"
+          alt="center-logo"
           className="center-Card-image"
         />
         <div className="left-card">
